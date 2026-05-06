894424bd082efdd4ad217d5a5b829492e8985261
DarkKandaoMaster <2837619550@qq.com>
2026-05-06 20:22:58 +0800

    src

--- QuickSay/main.cpp
+++ QuickSay/main.cpp
@@ -1364,8 +1364,7 @@ void showAdvancedInputHelp(QWidget & parent){
     </table><br>
     <p>组合快捷键说明</p>
     <ol>
-      <li>修饰键可以用<code>Ctrl</code>、<code>Alt</code>、<code>Shift</code>、<code>Win</code>。</li>
-      <li><code>Ctrl</code>可以写成<code>Control</code>；<code>Win</code>可以写成<code>Meta</code>或<code>Windows</code>。</li>
+      <li>修饰键可以写<code>Ctrl</code>（可以写成<code>Control</code>）、<code>Alt</code>、<code>Shift</code>、<code>Win</code>（可以写成<code>Meta</code>或<code>Windows</code>）。</li>
       <li>每个组合快捷键只能有一个主键。比如<code>&lt;press Ctrl+A&gt;</code>可以，<code>&lt;press A+B&gt;</code>不可以。</li>
     </ol>
 
