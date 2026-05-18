1a2791f68d2eb5a9e26f74a1f969c35bfb08fe85
DarkKandaoMaster <2837619550@qq.com>
2026-05-18 21:58:07 +0800

    src

--- QuickSay/main.cpp
+++ QuickSay/main.cpp
@@ -1281,7 +1281,7 @@ void showAdvancedInputHelp(QWidget & parent){
     <ol>
       <li>如果标签不符合规则，会原样输入。</li>
       <li>如果想原样输入<code>&lt;Enter&gt;</code>，请写成<code>\&lt;Enter&gt;</code>。</li>
-      <li>标签大小写不敏感。也就是说<code>&lt;Enter&gt;</code>可以写成<code>&lt;enter&gt;</code>。</li>
+      <li>标签不区分大小写。也就是说<code>&lt;Enter&gt;</code>可以写成<code>&lt;enter&gt;</code>。</li>
       <li>每执行一次高级输入，会按照设置里的“高级输入间隔”停顿一下。</li>
     </ol>
 
